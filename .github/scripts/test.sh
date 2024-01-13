2fc2bef61e19f4e33ca0e7ed89815c3dfa167a4f
oakguy1232 <67087158+dotheboogey678@users.noreply.github.com>
2024-01-12 20:58:01 -0600

    a

--- .github/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace Quake2-iOS.xcodeproj \
-            -scheme Quake2iOS\ iOS \
-            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 7 \
-            clean test | xcpretty
